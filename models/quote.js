971d6981e8b898d9a4f802560f6114ae8eaba030
jlb1999 <JacobBroughton@JacobBroughtonsMacbook.local>
2018-10-30 12:06:24 -0400

    revised commit

--- models/quote.js
+++ models/quote.js
@@ -5,25 +5,6 @@ module.exports = function(sequelize, DataTypes) {
     dayCode: DataTypes.STRING,
     serverTimestamp: DataTypes.STRING,
     mode: DataTypes.STRING,
-<<<<<<< HEAD
-    lastPrice: DataTypes.DECIMAL,
-    tradeTimestamp: DataTypes.STRING,
-    netChange: DataTypes.DECIMAL,
-    percentChange: DataTypes.DECIMAL,
-    unitCode: DataTypes.STRING,
-    open: DataTypes.DECIMAL,
-    high: DataTypes.DECIMAL,
-    low: DataTypes.DECIMAL,
-    close: DataTypes.DECIMAL,
-    numTrades: DataTypes.INTEGER,
-    dollarVolume: DataTypes.DECIMAL,
-    flag: DataTypes.STRING,
-    volume: DataTypes.INTEGER,
-    previousVolume: DataTypes.INTEGER,
-    fiftyTwoWkHigh: DataTypes.DECIMAL,
-    fiftyTwoWkHighDate: DataTypes.STRING,
-    fiftyTwoWkLow: DataTypes.DECIMAL,
-=======
     lastPrice: DataTypes.DECIMAL(10,4),
     tradeTimestamp: DataTypes.STRING,
     netChange: DataTypes.DECIMAL(10,4),
@@ -41,7 +22,6 @@ module.exports = function(sequelize, DataTypes) {
     fiftyTwoWkHigh: DataTypes.DECIMAL(10,4),
     fiftyTwoWkHighDate: DataTypes.STRING,
     fiftyTwoWkLow: DataTypes.DECIMAL(10,4),
->>>>>>> 0bf2764beb4db787e5bbb9a881e5db4bc80c24f0
     fiftyTwoWkLowDate: DataTypes.STRING
   });
   return Quote;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,64 +1,61 @@
-<<<<<<< HEAD
-  var db = require("../models");
-
-  // Able to search name and symbol
-  // Highest performer, lowest performer
+var db = require("../models");
+var rp = require("request-promise");
+// Able to search name and symbol
+// Highest performer, lowest performer
 module.exports = function (app) {
-  app.get("/api/stocks", function(req, res) {
-    db.Quote.findAll({}).then(function(result) {
+  app.get("/api/stocks", function (req, res) {
+    db.Quote.findAll({}).then(function (result) {
       res.json(result)
     })
   })
 
-  app.get("/api/stocks/:symbol", function(req, res) {
+  app.get("/api/stocks/:symbol", function (req, res) {
     db.Quote.findOne({
       where: {
-        symbol : req.params.symbol
+        symbol: req.params.symbol
       }
-    }).then(function(result) {
+    }).then(function (result) {
       res.json(result);
     })
   })
-  
-  app.get("/api/stocks/:name", function(req, res) {
+
+  app.get("/api/stocks/:name", function (req, res) {
     db.Quote.findOne({
       where: {
-        name : req.params.name
+        name: req.params.name
       }
-    }).then(function(result) {
+    }).then(function (result) {
       res.json(result)
     })
   })
 
-  app.get("/api/history/fb", function(req, res) {
-    db.FB.findAll({}).then(function(result) {
+  app.get("/api/history/fb", function (req, res) {
+    db.FB.findAll({}).then(function (result) {
       res.json(result)
     })
   })
 
-  app.get("/api/history/aapl", function(req, res) {
-    db.AAPL.findAll({}).then(function(result) {
+  app.get("/api/history/aapl", function (req, res) {
+    db.AAPL.findAll({}).then(function (result) {
       res.json(result)
     })
   })
 
-  app.get("/api/history/amzn", function(req, res) {
-    db.AMZN.findAll({}).then(function(result) {
+  app.get("/api/history/amzn", function (req, res) {
+    db.AMZN.findAll({}).then(function (result) {
       res.json(result)
     })
   })
 
-  app.get("/api/history/goog", function(req, res) {
-    db.GOOG.findAll({}).then(function(result) {
+  app.get("/api/history/goog", function (req, res) {
+    db.GOOG.findAll({}).then(function (result) {
       res.json(result)
     })
   })
-}
-=======
-var db = require("../models");
-var rp = require("request-promise");
 
-module.exports = function (app) {
+
+
+
   function createQuotes(arr) {
     var quote = arr.pop();
     if (!quote) {
@@ -323,4 +320,3 @@ module.exports = function (app) {
 }
 
 
->>>>>>> 0bf2764beb4db787e5bbb9a881e5db4bc80c24f0
